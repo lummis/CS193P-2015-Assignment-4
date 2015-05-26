7777c66bcfe93bbc84ce4f20b2fd15929e5f09da
Robert Lummis <robert.lummis@gmail.com>
2015-05-25 21:40:19 -0400

    next try to go back to defining scrollView in the storyboard, so I can have auto layout.

--- Smashtag/ImageVC.swift
+++ Smashtag/ImageVC.swift
@@ -43,53 +43,7 @@ class ImageVC: UIViewController, UIScrollViewDelegate {
         let scaleX = scrollView.bounds.size.width / originalImageSize.width
         let scaleY = (scrollView.bounds.size.height - navBarHeight) / originalImageSize.height
         scrollView.zoomScale = max(scaleX, scaleY)
-    }
-    
-//    override func viewDidAppear(animated: Bool) {
-//        super.viewDidAppear(animated)
-//        if imageView != nil {
-//            var imageV = imageView!
-//            originalImageSize = imageV.bounds.size
-//            println("imageView original frame: \(imageV.frame)")
-//            
-//            if let scrollViewSize = self.scrollView?.frame.size {
-//                let scaleX = scrollViewSize.width / originalImageSize.width
-//                let scaleY = scrollViewSize.height / originalImageSize.height
-//                var scale = min(scaleX, scaleY)
-//                let newWidth = originalImageSize.width * scale
-//                let newHeight = originalImageSize.height * scale
-//                let x = (scrollViewSize.width - newWidth) / 2
-//                let y = (scrollViewSize.height - newHeight) / 2
-//                imageV.frame = CGRectMake(x, y, newWidth, newHeight)
-//                scrollView.addSubview(imageV)
-//                scrollView.delegate = self
-//                scrollView.contentOffset = CGPointMake(0, 0)
-//            } else { println("no scrollView") }
-//        }
-//    }
-    
-//    func updateUI() {
-//        if imageView != nil {
-//            var imageV = imageView!
-//            let startSize = imageV.bounds.size
-//            println("imageView starting frame: \(imageV.frame)")
-//            
-//            if let scrollViewSize = scrollView?.frame.size {
-//                println("scrollView.frame: \(scrollView.frame)")
-//                let scaleX = scrollViewSize.width / originalImageSize.width
-//                let scaleY = scrollViewSize.height / originalImageSize.height
-//                let scale = min(scaleX, scaleY)
-//                scrollView.zoomScale = scale
-//                println("zoomScale: \(scale)")
-////                scrollView.addSubview(imageV)
-////                scrollView.delegate = self
-//            } else { println("no scrollView") }
-//        }
-//
-//    }
-    
-    func updateUI() {
-        
+        scrollView.contentOffset = CGPointMake(0, 0)
     }
     
     func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
@@ -107,10 +61,16 @@ class ImageVC: UIViewController, UIScrollViewDelegate {
         userDidScroll = true
     }
     
+    // scrollView doesn't get resized on rotate ???
     override func didRotateFromInterfaceOrientation(fromInterfaceOrientation: UIInterfaceOrientation) {
         println("did Rotate")
-//        view.setNeedsLayout()
-        updateUI()
+        let navBar = UINavigationController().navigationBar
+        let navBarHeight = navBar.bounds.size.height
+        println("navBarHeight: \(navBarHeight)")
+        scrollView.frame = CGRectMake(0, navBarHeight, view.frame.size.width, (view.frame.size.height - navBarHeight))
+        println("view frame: \(view.frame)")
+        println("scrollView frame: \(scrollView.frame)")
+        autoZoom()
     }
 
 }
